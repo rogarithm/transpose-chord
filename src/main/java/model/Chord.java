31fa8f8cc19bba578ecff7f81da5bda6060af7ce
sehun-kim <rogarithm@gmail.com>
2023-04-22 16:25:24 +0900

    같은 키 안의 1도와 2도 코드를 전조할 수 있다

--- src/main/java/model/Chord.java
+++ src/main/java/model/Chord.java
@@ -1,15 +1,37 @@
 package model;
 
 public class Chord {
+    private String key;
     private String bass;
+    private String other;
 
-    public Chord(String bass) {
-        this.bass = bass;
+    public Chord(String key, String chordString) {
+        if (chordString.length() == 1) {
+            this.key = key;
+            this.bass = chordString;
+        }
+        else if (chordString.length() > 1) {
+            this.key = key;
+            this.bass = chordString.substring(0, 1);
+            this.other = chordString.substring(1);
+        }
     }
 
-    public Chord transpose(String otherBass) {
-        this.bass = otherBass;
-        return this;
+    public Chord transpose(String toTranspose) {
+
+        if (this.bass == this.key) {
+            this.bass = toTranspose;
+            return this;
+        }
+        else {
+            String transposedBass = getTransposedBass(this.key, this.bass);
+            this.bass = transposedBass;
+            return this;
+        }
+    }
+
+    private String getTransposedBass(String key, String bass) {
+        return "B";
     }
 
     public String getBass() {
--- src/test/java/ChordTest.java
+++ src/test/java/ChordTest.java
@@ -6,9 +6,16 @@ public class ChordTest {
 
     @Test
     public void transposeOneChord() {
-        Chord gMajor = new Chord("G");
+        Chord gMajor = new Chord("G", "G");
         Chord aMajor = gMajor.transpose("A");
         Assertions.assertEquals(aMajor.getBass(), "A");
+    }
+
+    @Test
+    public void transposeSubDominantChord() {
+        Chord aMinor = new Chord("G", "Am");
+        Chord bMinor = aMinor.transpose("A");
+        Assertions.assertEquals(bMinor.getBass(), "B");
     }
 
 }
