894d46c3c994ca1a5f7d6d492e548c719de9af62
sehun-kim <rogarithm@gmail.com>
2023-05-01 15:41:40 +0900

    코드 하나를 전조할 때 Interval을 이용한다
     - Transposer의 doTranspose 연산 시 기존에 Note, Degree가 하던 연산을
       Interval이 대신한다.
     - 점진적으로 리팩토링하기 위해 먼저 doTranspose 메서드 구현을 바로
       바꾸지 않고, 메서드를 복사해 Interval을 쓰도록 바꿔서 테스트로 확인했다.

--- src/main/java/Transposer.java
+++ src/main/java/Transposer.java
@@ -1,10 +1,24 @@
 import model.Degree;
+import model.Interval;
 import model.Note;
 import model.Symbol;
 
 public class Transposer {
 
     public Transposer() {
+    }
+
+    public String doTransposeWithInterval(Symbol chord, Note currentKey, Note transposedKey) {
+        Interval itv = new Interval();
+
+        String rootNote = chord.getRootNote();
+        String other = chord.getOther();
+
+        Note note = Note.valueOf(rootNote);
+        int steps = itv.getInterval(currentKey, note);
+        Note transposedNote = itv.getRaisedNote(transposedKey, steps);
+
+        return transposedNote.toString() + other;
     }
 
     public String doTranspose(Symbol chord, Note currentKey, Note transposedKey) {
