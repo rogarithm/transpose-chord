32b3d11d09a9c3d72041d1a2fe553f45e374f298
sehun-kim <rogarithm@gmail.com>
2023-05-17 01:40:02 +0900

    Key 객체는 음 형식을 맞출 수 있다

--- src/main/java/model/Key.java
+++ src/main/java/model/Key.java
@@ -5,13 +5,38 @@ import model.note.NoteFactory;
 
 public class Key {
 
+    private enum EquivalentNote {
+        Db("Db", "C#"),
+        Eb("Eb", "D#"),
+        Gb("Gb", "F#"),
+        Ab("Ab", "G#"),
+        Bb("Bb", "A#");
+
+        private final String current;
+        private final String alternate;
+
+        EquivalentNote(String current, String next) {
+            this.current = current;
+            this.alternate = next;
+        }
+    }
+
     private final Note rootNote;
+    private final Interval interval;
 
     public Key(Note rootNote) {
         this.rootNote = rootNote;
+        this.interval = new Interval();
     }
 
     public Note formatNoteIn(Note note, String format) {
-        return NoteFactory.create("F#");
+        for (EquivalentNote equivalentNote : EquivalentNote.values()) {
+            if (equivalentNote.current.equals(note.toString()) &&
+            equivalentNote.alternate.substring(0,1).equals(format)) {
+                return NoteFactory.create(equivalentNote.alternate);
+            }
+        }
+
+        throw new IllegalArgumentException("can't format given note " + note + " in " + format + "!");
     }
 }
