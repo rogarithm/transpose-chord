807a622abcb2a4967e99e8352db19017c453b08e
sehun-kim <rogarithm@gmail.com>
2023-05-25 16:58:13 +0900

    중복되는 유효성 검증 로직을 클래스로 분리한다
     - Symbol과 NoteFactory에서 중복되는 유효성 검증 메서드를 클래스로
       만들어 분리했다.
     - NoteValidator 객체를 매번 만들지 않도록 정적 팩토리 메서드를
       추가했다.
     - 주어진 문자열이 유효한 음계인지 확인하는 로직을 추가했다.

--- src/main/java/model/Symbol.java
+++ src/main/java/model/Symbol.java
@@ -1,42 +1,29 @@
 package model;
 
+import model.note.NoteValidator;
+
 public class Symbol {
 
     private final String chord;
+    private static final NoteValidator validator = NoteValidator.getInstance();
 
     public Symbol(String chord) {
         this.chord = chord;
     }
 
     public String getRootNote() {
-        if (isNotFlat(chord) && isNotSharp(chord))
+        if (validator.isNotFlat(chord) && validator.isNotSharp(chord))
             return chord.substring(0, 1);
-        if (isFlat(chord) || isSharp(chord))
+        if (validator.isFlat(chord) || validator.isSharp(chord))
             return chord.substring(0, 2);
 
         throw new IllegalArgumentException("unable to get root note of given chord: " + chord);
     }
 
-    private boolean isNotFlat(String s) {
-        return (s.length() == 1) || (s.length() >= 2 && s.charAt(1) != 'b');
-    }
-
-    private boolean isFlat(String s) {
-        return s.length() >= 2 && s.charAt(1) == 'b';
-    }
-
-    private boolean isNotSharp(String s) {
-        return (s.length() == 1) || (s.length() >= 2 && s.charAt(1) != '#');
-    }
-
-    private boolean isSharp(String s) {
-        return s.length() >= 2 && s.charAt(1) == '#';
-    }
-
     public String getChordTones() {
-        if (isNotFlat(chord))
+        if (validator.isNotFlat(chord))
             return chord.substring(1);
-        if (isFlat(chord))
+        if (validator.isFlat(chord))
             return chord.substring(2);
 
         throw new IllegalArgumentException("unable to get given chord: " + chord);
--- src/main/java/model/note/NoteFactory.java
+++ src/main/java/model/note/NoteFactory.java
@@ -2,34 +2,19 @@ package model.note;
 
 public class NoteFactory {
 
+    private static final NoteValidator validator = NoteValidator.getInstance();
+
     public static Note create(String name) {
-        if (isNotFlat(name) && isNotSharp(name)) {
+        if (validator.isNotFlat(name) && validator.isNotSharp(name)) {
             return PlainNote.valueOf(name.substring(0, 1));
         }
-        if (isFlat(name)) {
+        if (validator.isFlat(name)) {
             return FlatNote.valueOf(name.substring(0, 1));
         }
-        if (isSharp(name)) {
+        if (validator.isSharp(name)) {
             return SharpNote.valueOf(name.substring(0, 1));
         }
 
         throw new IllegalArgumentException("unable to parse given chord " + name);
     }
-
-    private static boolean isNotFlat(String s) {
-        return (s.length() == 1) || (s.length() >= 2 && s.charAt(1) != 'b');
-    }
-
-    private static boolean isFlat(String s) {
-        return s.length() >= 2 && s.charAt(1) == 'b';
-    }
-
-    private static boolean isNotSharp(String s) {
-        return (s.length() == 1) || (s.length() >= 2 && s.charAt(1) != '#');
-    }
-
-    private static boolean isSharp(String s) {
-        return s.length() >= 2 && s.charAt(1) == '#';
-    }
-
 }
