4effc3ffc8fcb0ce2b2a00e4325d0ee6ffefb70e
sehun-kim <rogarithm@gmail.com>
2023-05-27 16:48:43 +0900

    메서드 실행 결과를 확인할 수 있게 테스트를 개선한다

--- src/test/java/LineParserTest.java
+++ src/test/java/LineParserTest.java
@@ -1,6 +1,9 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import model.Symbol;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,20 +12,39 @@ class LineParserTest {
 
     @Test
     public void splitChordsInOneLine() {
-        String line = "GM7 C Am7 D7sus4 G";
         LineParser parser = new LineParser(null);
+        String line = "GM7 C Am7 D7sus4 G";
+
         List<Symbol> chords = parser.splitChordsInLine(line);
-        Assertions.assertThat(chords.size()).isEqualTo(5);
+        List<Symbol> expectedResult = Stream.of("GM7", "C", "Am7", "D7sus4", "G")
+                                            .map(Symbol::new)
+                                            .collect(Collectors.toList());
+
+        for (int idx = 0; idx < chords.size(); idx++) {
+            String actual = chords.get(idx).toString();
+            String expected = expectedResult.get(idx).toString();
+            Assertions.assertThat(actual).isEqualTo(expected);
+            idx++;
+        }
     }
 
     @Test
     public void transposeChordsInOneLine() {
-        String line = "GM7 C Am7 D7sus4 G";
         LineParser parser = new LineParser(new Transposer("G", "D"));
+        String line = "GM7 C Am7 D7sus4 G";
+
         List<Symbol> chords = parser.splitChordsInLine(line);
-        List<Symbol> transposed = parser.transposeChordsInLine(chords);
-        for (Symbol chord : transposed) {
-            System.out.println(chord.getRootNote() + chord.getChordTones());
+        List<Symbol> transposedChords = parser.transposeChordsInLine(chords);
+
+        List<Symbol> expectedResult = Stream.of("DM7", "G", "Em7", "A7sus4", "D")
+                                            .map(Symbol::new)
+                                            .collect(Collectors.toList());
+
+        for (int idx = 0; idx < transposedChords.size(); idx++) {
+            String actual = transposedChords.get(idx).toString();
+            String expected = expectedResult.get(idx).toString();
+            Assertions.assertThat(actual).isEqualTo(expected);
+            idx++;
         }
     }
 }
