6d8337f2206641cbf28b2d0e8b12c358f611de03
sehun-kim <rogarithm@gmail.com>
2023-06-02 14:21:38 +0900

    FileHandler는 코드만 적힌 악보 전체를 처리할 수 있다
     - 코드만 있는 파일일 때, 파일 내 모든 코드를 전조할 수 있다.
     - 각각의 줄 처리를 Parser에 위임한다.

--- src/main/java/service/file/FileHandler.java
+++ src/main/java/service/file/FileHandler.java
@@ -5,8 +5,15 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import service.line.Parser;
 
 public class FileHandler {
+
+    private final Parser parser;
+
+    public FileHandler(Parser parser) {
+        this.parser = parser;
+    }
 
     public List<String> readFile(File file) {
         List<String> result = new ArrayList<>();
@@ -18,6 +25,24 @@ public class FileHandler {
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+        return result;
+    }
+
+    public List<String> handle(List<String> lines) {
+        List<String> result = new ArrayList<>();
+
+        for (String line : lines) {
+            StringBuilder oneLine = new StringBuilder();
+            List<String> parsedLine = parser.parseLine(line);
+
+            for (int i=0; i<parsedLine.size(); i++) {
+                oneLine.append(parsedLine.get(i));
+                if (i != parsedLine.size() - 1)
+                    oneLine.append(" ");
+            }
+            result.add(oneLine.toString());
+        }
+
         return result;
     }
 }
--- src/test/java/service/file/FileHandlerTest.java
+++ src/test/java/service/file/FileHandlerTest.java
@@ -1,21 +1,66 @@
 package service.file;
 
+import static org.mockito.Mockito.when;
+
 import java.io.File;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import service.line.Parser;
 
+@ExtendWith(MockitoExtension.class)
 class FileHandlerTest {
 
     private File file;
     private String pathName = "/Users/sehun/Documents/music";
     private String fileName = "chords.txt";
 
+    @InjectMocks
+    FileHandler handler;
+    @Mock
+    Parser parser;
+
+    @BeforeEach
+    public void setUp() {
+        file = new File(pathName + File.separator + fileName);
+        handler = new FileHandler(parser);
+    }
+
     @Test
     public void readFileOfGivenPath() {
-        file = new File(pathName + File.separator + fileName);
-        FileHandler handler = new FileHandler();
         List<String> lines = handler.readFile(file);
         Assertions.assertThat(lines.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void parseEveryLineInFile() {
+        List<String> lines = handler.readFile(file);
+        String line1 = "G Bm D C";
+        String line2 = "C D C G";
+        String line3 = "D C C Gmaj7";
+        String line4 = "Bbdim Am C";
+        List<String> parsedLine1 = Stream.of("G", "Bm", "D", "C")
+                                         .collect(Collectors.toList());
+        List<String> parsedLine2 = Stream.of("C", "D", "C", "G")
+                                         .collect(Collectors.toList());
+        List<String> parsedLine3 = Stream.of("D", "C", "C", "Gmaj7")
+                                         .collect(Collectors.toList());
+        List<String> parsedLine4 = Stream.of("Bbdim", "Am", "C")
+                                         .collect(Collectors.toList());
+        List<String> expectedResult = Stream.of("G Bm D C", "C D C G", "D C C Gmaj7", "Bbdim Am C")
+                                            .collect(Collectors.toList());
+
+        when(parser.parseLine(line1)).thenReturn(parsedLine1);
+        when(parser.parseLine(line2)).thenReturn(parsedLine2);
+        when(parser.parseLine(line3)).thenReturn(parsedLine3);
+        when(parser.parseLine(line4)).thenReturn(parsedLine4);
+        Assertions.assertThat(handler.handle(lines)).isEqualTo(expectedResult);
     }
 }
