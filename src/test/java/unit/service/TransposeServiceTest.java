1747de8fcf56a7100e7b80a885fd1692ddff2bf5
sehun-kim <rogarithm@gmail.com>
2023-06-13 23:09:43 +0900

    단위 테스트가 모의 객체를 쓰도록 고친다
     - TransposeServiceTest를 단위 테스트로 만들었지만 모의 객체가 아닌
       실제 객체를 쓰고 있었다.
     - @Mock으로 정의한 인스턴스에 행위를 정의하지 않았던 게 문제였고,
       고친 후 잘못 넣었던 실제 인스턴스 선언부는 지웠다.

--- src/test/java/unit/service/TransposeServiceTest.java
+++ src/test/java/unit/service/TransposeServiceTest.java
@@ -5,15 +5,12 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.List;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import service.TransposeService;
-import service.chord.Transposer;
-import service.line.LineParser;
 import service.util.FileHandler;
 import service.line.Parser;
 
@@ -32,20 +29,32 @@ class TransposeServiceTest {
 
     @Test
     public void parseEveryLineInFile() {
-        service = new TransposeService(
-                new LineParser(new Transposer("G", "G")),
-                new FileHandler(pathName, fileName)
-        );
+        service = new TransposeService(parser, handler);
+
+        List<String> readFromFile = Arrays.asList("G Bm D C", "C D C G", "D C C Gmaj7", "Am C");
+        when(handler.readFile()).thenReturn(readFromFile);
+
+        when(parser.parseLine(readFromFile.get(0))).thenReturn(Arrays.asList("G Bm D C"));
+        when(parser.parseLine(readFromFile.get(1))).thenReturn(Arrays.asList("C D C G"));
+        when(parser.parseLine(readFromFile.get(2))).thenReturn(Arrays.asList("D C C Gmaj7"));
+        when(parser.parseLine(readFromFile.get(3))).thenReturn(Arrays.asList("Am C"));
+
         List<String> expectedResult = Arrays.asList("G Bm D C", "C D C G", "D C C Gmaj7", "Am C");
         Assertions.assertThat(service.handle()).isEqualTo(expectedResult);
     }
 
     @Test
     public void parseEveryLineInFileToDifferentKey() {
-        service = new TransposeService(
-                new LineParser(new Transposer("G", "E")),
-                new FileHandler(pathName, fileName)
-        );
+        service = new TransposeService(parser, handler);
+
+        List<String> readFromFile = Arrays.asList("G Bm D C", "C D C G", "D C C Gmaj7", "Am C");
+        when(handler.readFile()).thenReturn(readFromFile);
+
+        when(parser.parseLine(readFromFile.get(0))).thenReturn(Arrays.asList("E G#m B A"));
+        when(parser.parseLine(readFromFile.get(1))).thenReturn(Arrays.asList("A B A E"));
+        when(parser.parseLine(readFromFile.get(2))).thenReturn(Arrays.asList("B A A Emaj7"));
+        when(parser.parseLine(readFromFile.get(3))).thenReturn(Arrays.asList("F#m A"));
+
         List<String> expectedResult = Arrays.asList("E G#m B A", "A B A E", "B A A Emaj7", "F#m A");
         Assertions.assertThat(service.handle()).isEqualTo(expectedResult);
     }
