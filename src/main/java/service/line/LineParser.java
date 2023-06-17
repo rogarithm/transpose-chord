a80e095da0904cbee8aba59ea5a9d0526a5770c0
sehun-kim <rogarithm@gmail.com>
2023-06-17 18:04:46 +0900

    메서드 위치를 바꾼다
     - parseLine() 안에서 쓰는 메서드 정의가 더 아래로 오도록 바꾼다.

--- src/main/java/service/line/LineParser.java
+++ src/main/java/service/line/LineParser.java
@@ -13,6 +13,18 @@ public class LineParser implements Parser {
         this.transposer = transposer;
     }
 
+    public List<String> parseLine(String line) {
+        List<Symbol> chords = collectChordsInLine(line);
+        List<Symbol> transposedChords = transposeChordsInLine(chords);
+
+        List<String> result = new ArrayList<>();
+        for (Symbol chord : transposedChords) {
+            result.add(chord.toString());
+        }
+
+        return result;
+    }
+
     public List<Symbol> collectChordsInLine(String line) {
         List<Symbol> result = new ArrayList<>();
         String[] splitted = line.split(" +");
@@ -30,18 +42,6 @@ public class LineParser implements Parser {
         for (Symbol chord : chords) {
             String transposed = transposer.doTranspose(chord.toString());
             result.add(new Symbol(transposed));
-        }
-
-        return result;
-    }
-
-    public List<String> parseLine(String line) {
-        List<Symbol> chords = collectChordsInLine(line);
-        List<Symbol> transposedChords = transposeChordsInLine(chords);
-
-        List<String> result = new ArrayList<>();
-        for (Symbol chord : transposedChords) {
-            result.add(chord.toString());
         }
 
         return result;
