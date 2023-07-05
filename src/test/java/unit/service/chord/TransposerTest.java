02b225e64b94fdd7c0e67cd541c051ec8e727344
sehun-kim <rogarithm@gmail.com>
2023-07-05 20:28:22 +0900

    Transposer 테스트 케이스를 추가한다
     - 기존 테스트에서 테스트하는 케이스가 적은 문제가 있었다. 하지만 각
       Note마다 동작을 확인하는 테스트 케이스를 만드는 건 테스트 클래스를
       번잡하게 만들 것이 예상되었다.
     - 이를 개선하기 위해 기존 테스트 케이스를 모두 지우고, 하나의 테스트
       케이스에서 동일한 키 안에 있는 모든 다이어토닉 코드의 전조를
       테스트하도록 바꿨다.

--- src/test/java/unit/service/chord/TransposerTest.java
+++ src/test/java/unit/service/chord/TransposerTest.java
@@ -1,40 +1,59 @@
 package unit.service.chord;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import model.Chord;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import service.chord.Transposer;
 
 class TransposerTest {
 
     @Test
-    public void transposeMajorChord() {
-        Transposer transposer = new Transposer("G", "A");
-        Chord transposed = transposer.doTranspose(new Chord("G"));
-        Assertions.assertThat(transposed).isEqualTo(new Chord("A"));
+    public void transposeFromFlatToPlain() {
+        Transposer transposer = new Transposer("Bb", "A");
+        List<Chord> chords = List.of("Bb", "C", "D", "Eb", "F", "G", "A")
+                                 .stream()
+                                 .map(Chord::new)
+                                 .collect(Collectors.toList());
+
+        List<Chord> transposed = new ArrayList<>();
+        for (Chord chord : chords) {
+            transposed.add(transposer.doTranspose(chord));
+        }
+
+        List<Chord> expected = List.of("A", "B", "C#", "D", "E", "F#", "G#")
+                                   .stream()
+                                   .map(Chord::new)
+                                   .collect(Collectors.toList());
+
+        for (int i = 0; i < transposed.size(); i++) {
+            assertThat(transposed.get(i)).isEqualTo(expected.get(i));
+        }
     }
 
     @Test
-    public void transposeMinorChord() {
-        Transposer transposer = new Transposer("G", "A");
-        Chord transposed = transposer.doTranspose(new Chord("Bm"));
-        Assertions.assertThat(transposed).isEqualTo(new Chord("C#m"));
-    }
-
-    @Test
-    public void transpose7thChord() {
-        Transposer transposer = new Transposer("G", "A");
-        Chord transposed = transposer.doTranspose(new Chord("D7"));
-        Assertions.assertThat(transposed).isEqualTo(new Chord("E7"));
-    }
-
-    @Test
-    public void t() {
+    public void transposeFromPlainToFlat() {
         Transposer transposer = new Transposer("A", "Bb");
+        List<Chord> chords = List.of("A", "B", "C#", "D", "E", "F#", "G#")
+                                 .stream()
+                                 .map(Chord::new)
+                                 .collect(Collectors.toList());
 
-        Chord transposed = transposer.doTranspose(new Chord("A"));
-        Chord expected = new Chord("Bb");
+        List<Chord> transposed = new ArrayList<>();
+        for (Chord chord : chords) {
+            transposed.add(transposer.doTranspose(chord));
+        }
 
-        Assertions.assertThat(transposed).isEqualTo(expected);
+        List<Chord> expected = List.of("Bb", "C", "D", "Eb", "F", "G", "A")
+                                   .stream()
+                                   .map(Chord::new)
+                                   .collect(Collectors.toList());
+
+        for (int i = 0; i < transposed.size(); i++) {
+            assertThat(transposed.get(i)).isEqualTo(expected.get(i));
+        }
     }
 }
