0acf0fef69cafc39da4cc565973dfd579f9663ae
sehun-kim <rogarithm@gmail.com>
2023-07-07 21:07:42 +0900

    테스트를 다듬는다
     - 공통인 변수를 하나로 모은다.

--- src/test/java/unit/service/chord/TransposerTest.java
+++ src/test/java/unit/service/chord/TransposerTest.java
@@ -6,28 +6,41 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import model.Chord;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.chord.Transposer;
 
 class TransposerTest {
 
+    List<Chord> notesInBflatKey;
+    List<Chord> notesInAKey;
+
+    @BeforeEach
+    public void setUp() {
+
+        notesInBflatKey = List.of("Bb", "C", "D", "Eb", "F", "G", "A")
+                              .stream()
+                              .map(Chord::new)
+                              .collect(Collectors.toList());
+
+        notesInAKey = List.of("A", "B", "C#", "D", "E", "F#", "G#")
+                          .stream()
+                          .map(Chord::new)
+                          .collect(Collectors.toList());
+    }
+
     @Test
     public void transposeFromFlatToPlain() {
+
         Transposer transposer = new Transposer("Bb", "A");
-        List<Chord> chords = List.of("Bb", "C", "D", "Eb", "F", "G", "A")
-                                 .stream()
-                                 .map(Chord::new)
-                                 .collect(Collectors.toList());
+        List<Chord> chords = notesInBflatKey;
 
         List<Chord> transposed = new ArrayList<>();
         for (Chord chord : chords) {
             transposed.add(transposer.doTranspose(chord));
         }
 
-        List<Chord> expected = List.of("A", "B", "C#", "D", "E", "F#", "G#")
-                                   .stream()
-                                   .map(Chord::new)
-                                   .collect(Collectors.toList());
+        List<Chord> expected = notesInAKey;
 
         for (int i = 0; i < transposed.size(); i++) {
             assertThat(transposed.get(i)).isEqualTo(expected.get(i));
@@ -36,21 +49,16 @@ class TransposerTest {
 
     @Test
     public void transposeFromPlainToFlat() {
+
         Transposer transposer = new Transposer("A", "Bb");
-        List<Chord> chords = List.of("A", "B", "C#", "D", "E", "F#", "G#")
-                                 .stream()
-                                 .map(Chord::new)
-                                 .collect(Collectors.toList());
+        List<Chord> chords = notesInAKey;
 
         List<Chord> transposed = new ArrayList<>();
         for (Chord chord : chords) {
             transposed.add(transposer.doTranspose(chord));
         }
 
-        List<Chord> expected = List.of("Bb", "C", "D", "Eb", "F", "G", "A")
-                                   .stream()
-                                   .map(Chord::new)
-                                   .collect(Collectors.toList());
+        List<Chord> expected = notesInBflatKey;
 
         for (int i = 0; i < transposed.size(); i++) {
             assertThat(transposed.get(i)).isEqualTo(expected.get(i));
