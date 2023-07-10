bdb900ec82596f3092775ac354e2bfc4f3bc5867
sehun-kim <rogarithm@gmail.com>
2023-07-10 17:35:10 +0900

    필드 이름을 다듬는다

--- src/main/java/service/chord/Transposer.java
+++ src/main/java/service/chord/Transposer.java
@@ -11,31 +11,32 @@ import model.note.NoteFactory;
 
 public class Transposer {
 
-    private final Note currentKey;
-    private final Note transposeTo;
+    private final Note currentRoot;
+    private final Note transposedRoot;
     private final Degree degree;
-    private final Key key;
+    private final Key transposedKey;
 
-    public Transposer(String currentKey, String transposeTo) {
-        this.currentKey = NoteFactory.create(currentKey);
-        this.transposeTo = NoteFactory.create(transposeTo);
-        this.degree = new Degree(this.transposeTo);
-        this.key = new Key(this.transposeTo);
+    public Transposer(String currentRoot, String transposedRoot) {
+        this.currentRoot = NoteFactory.create(currentRoot);
+        this.transposedRoot = NoteFactory.create(transposedRoot);
+        this.degree = new Degree(this.transposedRoot);
+        this.transposedKey = new Key(this.transposedRoot);
     }
 
     public Chord doTranspose(Chord chord) {
         Note currentBass = NoteFactory.create(chord.getRootNote());
 
         Interval interval = new Interval();
-        SemitoneCount semitones = interval.semitones(currentKey, currentBass);
-        Note tranposedBass = interval.raise(transposeTo, semitones);
+        SemitoneCount semitones = interval.semitones(currentRoot, currentBass);
 
+        Note tranposedBass = interval.raise(transposedRoot, semitones);
         DegreeNumber degreeNumber = interval.degree(semitones);
+
         Note resultBasis1 = degree.displayBasis(tranposedBass);
         Note resultBasis2 = degree.displayBasis(degreeNumber);
         boolean needConvertToSharp = !resultBasis1.equals(resultBasis2);
         if (needConvertToSharp) {
-            tranposedBass = key.convertToSharpNoteOfSamePitch(tranposedBass, resultBasis2);
+            tranposedBass = transposedKey.convertToSharpNoteOfSamePitch(tranposedBass, resultBasis2);
         }
 
         return new Chord(tranposedBass.toString() + chord.getChordTones());
